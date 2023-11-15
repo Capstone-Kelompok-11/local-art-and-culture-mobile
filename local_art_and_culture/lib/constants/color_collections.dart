32961257ed33ba047cc7a810a87d4f498a8f68d8
Rafi Ar Rafii <123966343+BangRF289@users.noreply.github.com>
2023-11-15 19:08:19 +0700

    submit

--- local_art_and_culture/lib/constants/color_collections.dart
+++ local_art_and_culture/lib/constants/color_collections.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 class ColorCollections {
   static const Color primaryColor = Color(0xFF3653B0);
   static const Color secondaryColor = Color(0xFFE8644B);
+  static const Color grseenColor = Color(0xFF026C52);
 }
