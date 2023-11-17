6bddabde6aff707b9f9aedea23741cf5b9d61021
Yustikaprwt <83440868+Yustikaprwt@users.noreply.github.com>
2023-11-17 09:55:32 +0800

    Add picture assets and product page

---
+++ local_art_and_culture/lib/src/feature/product/product_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class ProductPage extends StatefulWidget {
+  const ProductPage({Key? key}) : super(key: key);
+
+  @override
+  State<ProductPage> createState() => _ProductPageState();
+}
+
+class _ProductPageState extends State<ProductPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Product Page'),
+          ],
+        ),
+      ),
+    );
+  }
+}
