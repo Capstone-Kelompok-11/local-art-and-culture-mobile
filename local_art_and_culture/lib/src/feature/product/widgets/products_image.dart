2160448b2623ecfe54aa71b51a19db13ee31e029
Yustikaprwt <83440868+Yustikaprwt@users.noreply.github.com>
2023-11-30 17:59:35 +0800

    Memperbaiki UI detail gambar produk

--- local_art_and_culture/lib/src/feature/product/widgets/products_image.dart
+++ local_art_and_culture/lib/src/feature/product/widgets/products_image.dart
@@ -13,112 +13,138 @@ class _ProductsImageState extends State<ProductsImage> {
   bool isFavorite = false;
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Stack(
-          children: [
-            const Image(image: AssetImage('lib/assets/gambar-produk-2.jpg')),
-            Positioned(
-              top: 23,
-              left: 20,
-              child: IconButton(
-                icon: const Icon(
-                  Icons.keyboard_backspace_rounded,
-                  color: Colors.black,
-                  size: 28.0,
-                ),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const ListProductPage()),
-                  );
-                },
-              ),
-            ),
-            Positioned(
-              top: 20,
-              right: 20,
-              child: GestureDetector(
-                onTap: () {
-                  setState(() {
-                    isFavorite = !isFavorite;
-                  });
-                },
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(25),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(5.0),
-                    child: Icon(
-                      isFavorite
-                          ? Icons.favorite_rounded
-                          : Icons.favorite_border_rounded,
-                      color: isFavorite ? Colors.red : const Color(0xff999999),
-                      size: 28.0,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+    return Container(
+      color: Colors.white,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Stack(
             children: [
-              const SizedBox(height: 10),
-              Text('Baju Kebaya Wanita Pink Full Set Lokal',
-                  style: GoogleFonts.plusJakartaSans(
-                      fontSize: 18, fontWeight: FontWeight.w600)),
-              const SizedBox(height: 8),
-              Text('Rp 180.000',
-                  style: GoogleFonts.plusJakartaSans(
-                      fontSize: 24,
-                      fontWeight: FontWeight.w700,
-                      color: const Color(0xff3653B0))),
-              const SizedBox(height: 8),
-              Row(
-                children: [
-                  const Icon(
-                    Icons.star_rounded,
-                    color: Color(0xfff3b502),
-                    size: 17.0,
+              const Image(image: AssetImage('lib/assets/gambar-produk-2.jpg')),
+              Positioned(
+                top: 23,
+                left: 20,
+                child: IconButton(
+                  icon: const Icon(
+                    Icons.keyboard_backspace_rounded,
+                    color: Colors.black,
+                    size: 28.0,
                   ),
-                  const SizedBox(width: 4.0),
-                  RichText(
-                    text: TextSpan(
-                      text: '4.9',
-                      style: GoogleFonts.plusJakartaSans(
-                        fontSize: 14,
-                        fontWeight: FontWeight.w400,
-                        color: Colors.black,
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const ListProductPage()),
+                    );
+                  },
+                ),
+              ),
+              Positioned(
+                top: 20,
+                right: 20,
+                child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      isFavorite = !isFavorite;
+                    });
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: const Color(0xffB3B3B3),
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(6.0),
+                      child: Icon(
+                        isFavorite
+                            ? Icons.favorite_rounded
+                            : Icons.favorite_border_rounded,
+                        color:
+                            isFavorite ? Colors.white : const Color(0xff999999),
+                        size: 28.0,
                       ),
-                      children: [
-                        const WidgetSpan(
-                          child: SizedBox(width: 10.0),
-                        ),
-                        TextSpan(
-                          text: '153 Terjual',
-                          style: GoogleFonts.plusJakartaSans(
-                            fontSize: 14,
-                            fontWeight: FontWeight.w400,
-                            color: const Color(0xff999999),
-                          ),
-                        ),
-                      ],
                     ),
                   ),
-                ],
-              )
+                ),
+              ),
             ],
           ),
-        ),
-      ],
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0, vertical: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Container(
+                    height: 20,
+                    width: 90,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                      color: const Color(0xffF6C63C),
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Karya Tangan',
+                        style: GoogleFonts.plusJakartaSans(
+                          fontSize: 10,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Text('Baju Kebaya Wanita Pink Full Set Lokal',
+                    style: GoogleFonts.plusJakartaSans(
+                        fontSize: 18, fontWeight: FontWeight.w600)),
+                const SizedBox(height: 8),
+                Text('Rp 180.000',
+                    style: GoogleFonts.plusJakartaSans(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w700,
+                        color: const Color(0xff3653B0))),
+                const SizedBox(height: 8),
+                Row(
+                  children: [
+                    const Icon(
+                      Icons.star_rounded,
+                      color: Color(0xfff3b502),
+                      size: 17.0,
+                    ),
+                    const SizedBox(width: 4.0),
+                    RichText(
+                      text: TextSpan(
+                        text: '4.9',
+                        style: GoogleFonts.plusJakartaSans(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.black,
+                        ),
+                        children: [
+                          const WidgetSpan(
+                            child: SizedBox(width: 10.0),
+                          ),
+                          TextSpan(
+                            text: '153 Terjual',
+                            style: GoogleFonts.plusJakartaSans(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w400,
+                              color: const Color(0xff999999),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
