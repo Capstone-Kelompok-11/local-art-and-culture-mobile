bb57307fcdd733aaf59bc8bedebb2b2b8dcd5a2a
taqichan <taqibuqu@gmail.com>
2023-12-06 22:04:59 +0700

    chatbot dan bantuan clear

--- local_art_and_culture/lib/main.dart
+++ local_art_and_culture/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:local_art_and_culture/src/feature/chatbot/bantuan_screen.dart';
 
 import 'package:local_art_and_culture/src/feature/chatbot/chatbot_screen.dart';
 
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ChatbotScreen(),
+      home: BantuanScreen(),
     );
   }
-
+}
