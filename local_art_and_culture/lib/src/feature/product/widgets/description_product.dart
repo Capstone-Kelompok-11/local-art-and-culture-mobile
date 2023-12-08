73977e87ce0cc1bdf5f6fec4a1f068a44ccfddd3
Yustikaprwt <83440868+Yustikaprwt@users.noreply.github.com>
2023-12-08 10:29:10 +0800

    Menambahkan dialog bottom sheet pada deskripsi produk di halaman detail produk

--- local_art_and_culture/lib/src/feature/product/widgets/description_product.dart
+++ local_art_and_culture/lib/src/feature/product/widgets/description_product.dart
@@ -9,8 +9,6 @@ class DescriptionProducts extends StatefulWidget {
 }
 
 class _DescriptionProductsState extends State<DescriptionProducts> {
-  bool isExpanded = false;
-
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -28,26 +26,111 @@ class _DescriptionProductsState extends State<DescriptionProducts> {
                         fontSize: 18, fontWeight: FontWeight.w700)),
                 const SizedBox(height: 8),
                 Text(
-                  'Lorem ipsum dolor sit amet consectetur. Amet varius turpis habitasse tempus. Eros eu aliquet enim rutrum etiam venenatis dolor tortor.',
+                  'Dengan Mutiara Khas Bali, kamu tidak hanya mendapatkan perhiasan, tetapi juga mengenakan potongan seni yang menggambarkan warisan dan...',
                   style: GoogleFonts.plusJakartaSans(
                       fontSize: 16,
                       fontWeight: FontWeight.w400,
                       color: const Color(0xff666666)),
-                  maxLines: isExpanded ? null : 3,
-                  overflow: TextOverflow.ellipsis,
                 ),
                 const SizedBox(height: 8),
                 InkWell(
                   onTap: () {
-                    setState(() {
-                      isExpanded = !isExpanded;
-                    });
+                    showModalBottomSheet(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return SingleChildScrollView(
+                          child: Container(
+                            height: 650,
+                            decoration: const BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(25.0),
+                                topRight: Radius.circular(25.0),
+                              ),
+                            ),
+                            padding: const EdgeInsets.all(16.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  children: [
+                                    IconButton(
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                      },
+                                      icon: const Icon(
+                                        Icons.clear_sharp,
+                                        size: 24,
+                                      ),
+                                    ),
+                                    Text(
+                                      'Deskripsi Produk',
+                                      style: GoogleFonts.plusJakartaSans(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.w700,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                const SizedBox(height: 20),
+                                Text(
+                                  'Dengan Mutiara Khas Bali, Anda tidak hanya mendapatkan perhiasan, tetapi juga mengenakan potongan seni yang menggambarkan warisan dan keindahan alam Pulau Dewata. Hadirkan kilau eksklusif ini dalam setiap momen Anda, dan rasakan keunggulan produk kami yang melebihi harapan.',
+                                  style: GoogleFonts.plusJakartaSans(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w400,
+                                    color: const Color(0xff666666),
+                                  ),
+                                ),
+                                const SizedBox(height: 30),
+                                Text(
+                                  'Alasan membeli produk kami:\n'
+                                  '- Mutiara asli berkualitas tinggi\n'
+                                  '- Desain tradisional Bali yang elegan\n'
+                                  '- Karya tangan yang terampil\n'
+                                  '- Kemasan mewah\n'
+                                  '- Ketahanan dan keabadian',
+                                  style: GoogleFonts.plusJakartaSans(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w400,
+                                    color: const Color(0xff666666),
+                                  ),
+                                ),
+                                const SizedBox(height: 30),
+                                Text(
+                                  'Spesifikasi:\n'
+                                  '- Bahan: Alloy\n'
+                                  '- Ukuran Gelang: 40x5 cm\n'
+                                  '- Kategori: Karya Tangan',
+                                  style: GoogleFonts.plusJakartaSans(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w400,
+                                    color: const Color(0xff666666),
+                                  ),
+                                ),
+                                const SizedBox(height: 30),
+                                Text(
+                                  'Isi Set: Sepasang Anting Mutiara Khas Bali dan Gelang Mutiara Khas Bali',
+                                  style: GoogleFonts.plusJakartaSans(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w400,
+                                    color: const Color(0xff666666),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    );
                   },
-                  child: Text(isExpanded ? 'Baca Selengkapnya' : 'Sembunyikan',
-                      style: GoogleFonts.plusJakartaSans(
-                          color: const Color(0xff637DCF),
-                          fontWeight: FontWeight.w600,
-                          fontSize: 16)),
+                  child: Text(
+                    'Baca Selengkapnya',
+                    style: GoogleFonts.plusJakartaSans(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w700,
+                      color: const Color(0xff637DCF),
+                    ),
+                  ),
                 ),
               ],
             ),
