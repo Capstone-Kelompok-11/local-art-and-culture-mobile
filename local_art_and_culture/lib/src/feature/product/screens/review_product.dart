182f22a97113756ecb9125621314ef553d32d727
Yustikaprwt <83440868+Yustikaprwt@users.noreply.github.com>
2023-12-09 14:52:08 +0800

    Memperbaiki desain UI halaman review produk

--- local_art_and_culture/lib/src/feature/product/screens/review_product.dart
+++ local_art_and_culture/lib/src/feature/product/screens/review_product.dart
@@ -1,5 +1,7 @@
+// ignore_for_file: use_full_hex_values_for_flutter_colors
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:local_art_and_culture/src/feature/product/widgets/review_product_person.dart';
 
 class ReviewProductPage extends StatefulWidget {
   const ReviewProductPage({Key? key}) : super(key: key);
@@ -12,6 +14,7 @@ class _ReviewProductPageState extends State<ReviewProductPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xff6e8e8e8),
       appBar: AppBar(
         title: Text(
           'Checkout',
@@ -23,88 +26,46 @@ class _ReviewProductPageState extends State<ReviewProductPage> {
         centerTitle: true,
       ),
       body: ListView.builder(
-        itemCount: 3,
-        itemBuilder: (context, index) {
-          return Container(
-            color: Colors.white,
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+        itemCount: 4,
+        itemBuilder: (BuildContext context, int index) {
+          if (index == 0) {
+            return Container(
+                height: 40,
+                color: Colors.white,
+                child: Row(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.only(left: 15),
+                      child: Icon(
+                        Icons.star_rate_rounded,
+                        color: Color(0xfff3b502),
+                      ),
+                    ),
+                    const SizedBox(width: 5),
+                    Text('4.9',
+                        style: GoogleFonts.plusJakartaSans(
+                            fontSize: 18, fontWeight: FontWeight.w700)),
+                    Text('/5.0',
+                        style: GoogleFonts.plusJakartaSans(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                            color: const Color(0xff999999))),
+                    const SizedBox(width: 210),
+                    Text('(60 Ulasan)',
+                        style: GoogleFonts.plusJakartaSans(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                            color: const Color(0xff999999)))
+                  ],
+                ));
+          } else {
+            return const Column(
               children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 16.0, vertical: 10),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(30.0),
-                        child: const Image(
-                          image: AssetImage('assets/logo-store.jpg'),
-                          width: 25,
-                          height: 25,
-                        ),
-                      ),
-                      const SizedBox(width: 15),
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              'John Wick',
-                              style: GoogleFonts.plusJakartaSans(
-                                fontSize: 15,
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                            const SizedBox(height: 5),
-                            Row(
-                              children: List.generate(
-                                4,
-                                (index) => const Icon(
-                                  Icons.star_rounded,
-                                  color: Color(0xfff3b502),
-                                  size: 23.0,
-                                ),
-                              ),
-                            ),
-                            const SizedBox(height: 8),
-                            Text(
-                              'Ukuran: All Size',
-                              style: GoogleFonts.plusJakartaSans(
-                                fontSize: 15,
-                                fontWeight: FontWeight.w500,
-                                color: const Color(0xffB3B3B3),
-                              ),
-                            ),
-                            const SizedBox(height: 8),
-                            Text(
-                              'Mantap banget kebayanya, warnanya elegan. Muat juga diaku ukurannya pas sekali. barang sesuai gambar yang tertera',
-                              style: GoogleFonts.plusJakartaSans(
-                                fontSize: 16,
-                                fontWeight: FontWeight.w400,
-                                height: 21 / 16,
-                              ),
-                            ),
-                            const SizedBox(height: 8),
-                            ClipRRect(
-                              borderRadius: BorderRadius.circular(10.0),
-                              child: const Image(
-                                image: AssetImage('assets/gambar-produk-2.jpg'),
-                                width: 90,
-                                height: 90,
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
+                SizedBox(height: 8),
+                ReviewProductByPerson(),
               ],
-            ),
-          );
+            );
+          }
         },
       ),
     );
