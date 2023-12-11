0067c31d8f83f50b1250ede56ce31abb75ae79af
Rafi Ar Rafii <123966343+BangRF289@users.noreply.github.com>
2023-12-11 17:33:41 +0700

    penambahan event

--- local_art_and_culture/lib/src/feature/event/pameran/screens/main_screen.dart
+++ local_art_and_culture/lib/src/feature/event/pameran/screens/main_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:local_art_and_culture/components/bottom_navigation_bar.dart';
 import 'package:local_art_and_culture/src/feature/event/pameran/screens/event_screen.dart';
-
-
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -18,101 +17,23 @@ class _MainScreenState extends State<MainScreen> {
     Scaffold(),
     Scaffold(),
   ];
-  
+
   get primaryBlue70Color => null;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomAppBar(
-        color: Colors.white,
-        height: 70,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            GestureDetector(
-              onTap: () => setState(() {
-                currentTab = 0;
-              }),
-              child: Column(
-                children: [
-                  Icon(
-                    currentTab == 0 ? Icons.home : Icons.home,
-                    color: currentTab == 0 ? primaryBlue70Color : Colors.grey,
-                  ),
-                  Text(
-                    "Home",
-                    style: TextStyle(
-                      fontSize: 14,
-                      color: currentTab == 0 ? primaryBlue70Color : Colors.grey,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            GestureDetector(
-              onTap: () => setState(() {
-                currentTab = 1;
-              }),
-              child: Column(
-                children: [
-                  Icon(
-                    currentTab == 1 ? Icons.event : Icons.event,
-                    color: currentTab == 1 ? primaryBlue70Color : Colors.grey,
-                  ),
-                  Text(
-                    "Event",
-                    style: TextStyle(
-                      fontSize: 14,
-                      color: currentTab == 1 ? primaryBlue70Color : Colors.grey,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            GestureDetector(
-              onTap: () => setState(() {
-                currentTab = 2;
-              }),
-              child: Column(
-                children: [
-                  Icon(
-                    currentTab == 2 ? Icons.event : Icons.event,
-                    color: currentTab == 2 ? primaryBlue70Color : Colors.grey,
-                  ),
-                  Text(
-                    "Product",
-                    style: TextStyle(
-                      fontSize: 14,
-                      color: currentTab == 2 ?primaryBlue70Color : Colors.grey,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            GestureDetector(
-              onTap: () => setState(() {
-                currentTab = 3;
-              }),
-              child: Column(
-                children: [
-                  Icon(
-                    currentTab == 3 ? Icons.person : Icons.person,
-                    color: currentTab == 3 ? primaryBlue70Color : Colors.grey,
-                  ),
-                  Text(
-                    "Profile",
-                    style: TextStyle(
-                      fontSize: 14,
-                      color: currentTab == 3 ? primaryBlue70Color: Colors.grey,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
+      bottomNavigationBar: CustomBottomNavigationBar(
+          selectedIndex: 1,
+          onItemTapped: (index) {
+            if (index == 1) {
+              Navigator.pushNamed(context, '/event');
+            } else if (index == 2) {
+              Navigator.pushNamed(context, '/product');
+            } else if (index == 3) {
+              Navigator.pushNamed(context, '/profile');
+            }
+          }),
       body: screens[currentTab],
     );
   }
